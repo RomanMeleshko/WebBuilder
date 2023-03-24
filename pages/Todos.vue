eb92bfbb99fbf02d2226db02838a560faddf87cc
Roman <roman@macbook.local>
2023-03-24 16:18:50 +0200

    feature/2 Added new pages

---
+++ pages/Todos.vue
@@ -0,0 +1,29 @@
+<template>
+   <div id="Todos">
+      <div class="todos">
+         <div class="wrapper-home">
+            <div class="container">
+              <p>THIS PAGE OF TODOS</p>
+            </div>
+         </div>
+
+      </div>
+
+   </div>
+</template>
+
+<script>
+
+ export default {
+   name: "Todos",
+   data() {
+     return {
+
+     }
+   }
+ }
+</script>
+
+<style scoped>
+
+</style>
