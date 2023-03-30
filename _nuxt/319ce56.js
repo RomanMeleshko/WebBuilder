2fe6f30946a3a36ed14967c4794ced77d10e0ed1
Roman <roman@macbook.local>
2023-03-30 22:38:18 +0300

    deploy/5 New fix for Welcome page

---
+++ _nuxt/319ce56.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[5],{372:function(n,t,e){"use strict";e.r(t);var l=e(55),component=Object(l.a)({},(function(){var n=this._self._c;return n("div",{attrs:{id:"Home"}},[n("div",{staticClass:"Home"},[this._v("\n    THIS HOME PAGE\n    "),n("nuxt")],1)])}),[],!1,null,null,null);t.default=component.exports}}]);
